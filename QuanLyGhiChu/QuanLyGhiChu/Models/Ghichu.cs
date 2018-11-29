ea971b048b8f30165fecb6e02fc16f6ce22c122c
Tuan Le <42384687+ltt2897@users.noreply.github.com>
2018-11-29 23:58:31 +0700

    Add Connection Strings, Scaffold-DbContext

---
+++ QuanLyGhiChu/QuanLyGhiChu/Models/Ghichu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuanLyGhiChu.Models
+{
+    public partial class Ghichu
+    {
+        public int Id { get; set; }
+        public string HashCode { get; set; }
+        public string Token { get; set; }
+        public string Title { get; set; }
+        public string Context { get; set; }
+        public DateTime TimeCreated { get; set; }
+        public DateTime? TimeUpdated { get; set; }
+        public byte HienAn { get; set; }
+    }
+}
--- QuanLyGhiChu/QuanLyGhiChu/Startup.cs
+++ QuanLyGhiChu/QuanLyGhiChu/Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using QuanLyGhiChu.Models;
 
 namespace QuanLyGhiChu
 {
@@ -23,6 +25,9 @@ namespace QuanLyGhiChu
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<QuanLyGhiChuContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("QuanLyGhiChu"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
