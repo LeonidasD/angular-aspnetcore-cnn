f65044934c71eafb92dcf28deb6fe4b915757003
Tuan Le <42384687+ltt2897@users.noreply.github.com>
2018-11-30 20:27:00 +0700

    Up test

---
+++ QuanLyGhiChu/QuanLyGhiChu/Controllers/GhiChuApiController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.CodeGeneration;
+using Newtonsoft.Json;
+using QuanLyGhiChu.Models;
+
+namespace QuanLyGhiChu.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/GhiChu")]
+    public class GhiChuApiController : ControllerBase
+    {
+        private readonly QuanLyGhiChuContext _context;
+
+        public async Task<IActionResult> Create(string title, string context)
+        {
+            string md5String = Encoding.ASCII.GetString(MD5.Create().ComputeHash(Encoding.ASCII.GetBytes(title)));
+            DateTime dt = DateTime.Now;
+            string timeNow = dt.ToString("yyMdhhmm");
+
+            var item = new Ghichu
+            {
+                HashCode = md5String.Substring(0, 6) + "_" + timeNow,
+                Token = md5String,
+                Title = title,
+                Context = context,
+                TimeCreated = dt,
+                TimeUpdated = null,
+                HienAn = 1
+            };
+
+            _context.Ghichu.Add(item);
+            await _context.SaveChangesAsync();
+
+            return  JsonConvert.
+        }
+    }
+}
