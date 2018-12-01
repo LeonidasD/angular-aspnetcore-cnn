2d198903bae6c6d3d9bd70acb21c9a6cb005764c
DUC\MyPC <duc.huu128@gmail.com>
2018-12-01 12:24:53 +0700

    Config POST method Angular

--- QuanLyGhiChu/QuanLyGhiChu/ClientApp/app/components/home/home.component.ts
+++ QuanLyGhiChu/QuanLyGhiChu/ClientApp/app/components/home/home.component.ts
@@ -9,10 +9,18 @@ import { Http } from '@angular/http';
 })
 export class HomeComponent {
     http: Http;
+
     constructor(http: Http, @Inject('BASE_URL') baseurl: string) {
         this.http = http;
     }
     public postCreateNote(f: NgForm) {
-        this.http.post('api/GhiChu/Create', f.value).subscribe();
+        this.http.post('api/GhiChu/Create', f.value).subscribe(
+            data => {
+                if (data.status === 200) {
+                    
+                }
+            },
+            error => console.log("Error")
+        )
     }
 }
